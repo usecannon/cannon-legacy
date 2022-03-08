4f972a11de9f85c2014d7a9f008117616588d877
Matías <mjlescano@protonmail.com>
2022-03-08 15:43:33 -0600

    add ERC20 cannonfile

--- packages/contracts/contracts/Imports.sol
+++ packages/contracts/contracts/Imports.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
